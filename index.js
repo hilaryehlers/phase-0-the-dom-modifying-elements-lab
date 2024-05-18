af898fa33aa02786c7e19ea54f718c75c8ec0ccd
Hilary Ehlers <hilarydixson@gmail.com>
2024-05-18 16:33:44 -0500

    modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>Hilary is the champion</h1>"
+element.append(h1);
